3e1685400159b88964bc513d33714fd803eac609
Cepik-arch <cepicz@email.cz>
2024-03-13 20:28:14 +0100

    Update DeclarationBlock.cs

--- new/DeclarationBlock.cs
+++ new/DeclarationBlock.cs
@@ -9,6 +9,8 @@ public class DeclarationBlock : Block
     public TMP_InputField variableName; // Name of the variable to declare
     public TMP_InputField initialValue; // Initial value of the variable
 
+
+
     protected override void Awake()
     {
         base.Awake();
@@ -38,13 +40,28 @@ public class DeclarationBlock : Block
 
     private void DeclareVariable(string name, int initialValue)
     {
-        // Implement logic to declare the variable
-        // For simplicity, we'll just log the declaration here
-        Debug.Log($"Declared variable: {name} with initial value: {initialValue}");
+        // Check if the variable with the same name already exists
+        bool variableExists = false;
+        foreach (var value in values)
+        {
+            if (value.variable == name)
+            {
+                variableExists = true;
+                value.value = initialValue; // Update the value of the existing variable
+                Debug.Log($"Updated variable: {name} with new value: {initialValue}");
+                break;
+            }
+        }
 
-        var newValue = new Value() { variable = name, value = initialValue };
-        values.Add(newValue);
-        ShowValues();
+        // If the variable doesn't exist, add it to the list
+        if (!variableExists)
+        {
+            Debug.Log($"Declared variable: {name} with initial value: {initialValue}");
+            var newValue = new Value() { variable = name, value = initialValue };
+            values.Add(newValue);
+        }
+
+        ShowValues(); // Show the updated list of variables
     }
 
     public void PopulateDropdownOptions()
